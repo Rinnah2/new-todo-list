64b27b9e21ac86ffedd66843d79a80c28bf979f7
Rinnah Mills <rinnahmills06@gmail.com>
2026-05-19 22:47:38 +0000

    Added dialogue box for new tasks

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ Search Bar
 List Of Tasks
 */
 
+// 1. Grab both elements
+let openFormBtn = document.querySelector('.addTask');
+let dialogueBox = document.getElementById('dialogueBox');
+
+// 2. Add the click event listener to the main button
+openFormBtn.addEventListener('click', function() {
+
+    // 3. Remove the 'hidden' class to show the box
+    dialogueBox.classList.remove('hidden');
+
+});
